acffbddf547ce8f9396f768b5f35e9c0b2054e03
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2022-04-21 14:53:40 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -3,11 +3,14 @@ const title = document.getElementById("text");
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.height = window.innerHeight
+let headShot = new Image()
+headShot.src = 'Vicky head shot.jpeg'
 let particlesArray = [];
+let photoParticlesArray = [];
 let numberOfParticles = 150;
+let numberOfPhotoParticles = 4;
 let titleMeasurement = title.getBoundingClientRect();
-console.log(titleMeasurement);
 
 let titleBox = {
   x: titleMeasurement.left,
@@ -15,6 +18,35 @@ let titleBox = {
   width: titleMeasurement.width,
   height: 10,
 };
+
+class PhotoParticle {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.size = Math.random() * 16 + 1;
+    this.weight = Math.random() * .005 + .005;
+    this.directionX = -2.2
+    this.hue = Math.random() * 360 + 1;
+  }
+  update() {
+    if (this.y > canvas.height) {
+      this.y = 0;
+      this.weight = Math.random() * 1 + 1;
+      this.x = Math.random() * canvas.width * 1.2;
+    }
+    this.weight += 0.005;
+    this.y += this.weight;
+    this.x += this.directionX;
+
+    if (this.x + (this.size + 100) > canvas.width || this.x < 0) {
+      this.directionX *= -1;
+    }
+  }
+
+  draw() {
+    ctx.drawImage(headShot, this.x, this.y, 100, 100);
+  }
+}
 
 class Particle {
   constructor(x, y) {
@@ -58,6 +90,7 @@ class Particle {
     ctx.fill();
   }
 }
+
 function init() {
   particlesArray = [];
   for (let i = 0; i < numberOfParticles; i++) {
@@ -65,44 +98,30 @@ function init() {
     let y = Math.random() * canvas.height;
     particlesArray.push(new Particle(x, y));
   }
+   photoParticlesArray = [];
+   for (let i = 0; i < numberOfPhotoParticles; i++) {
+     let x = Math.random() * canvas.width;
+     let y = Math.random() * canvas.height;
+     photoParticlesArray.push(new PhotoParticle(x, y));
+   }
 }
+
 init();
-function connect() {
-  // let hue = Math.floor(Math.random()* 360 + 1) * .4;
-  for (let a = 0; a < particlesArray.length; a++) {
-    const particle = particlesArray[a];
-
-    for (let b = a + 1; b < particlesArray.length; b++) {
-      const other = particlesArray[b];
-
-      let dx = particle.x - other.x;
-      let dy = particle.y - other.y;
-      let distance = Math.sqrt(dx * dx + dy * dy);
-
-      if (distance < 90) {
-        ctx.strokeStyle = 'hsl(278, 50%, 50%)'
-        ctx.beginPath();
-        ctx.lineWidth = 4;
-        ctx.moveTo(particle.x, particle.y);
-        ctx.lineTo(other.x, other.y);
-        ctx.stroke();
-        ctx.closePath;
-      }
-    }
-  }
-}
 
 function animate() {
   //ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = "rgba(255, 255, 255, 0.01)";
+  ctx.fillStyle = "rgba(0, 0, 0, 0.1)";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
   for (let i = 0; i < particlesArray.length; i++) {
     particlesArray[i].update();
     particlesArray[i].draw();
   }
-  connect();
-  ctx.fillRect(titleBox.x, titleBox.y, titleBox.width, titleBox.height);
+  for (let i = 0; i < photoParticlesArray.length; i++) {
+    photoParticlesArray[i].update();
+    photoParticlesArray[i].draw();
+  }
+  
   requestAnimationFrame(animate);
 }
 
